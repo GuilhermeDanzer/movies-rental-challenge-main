0a344d14b37da6aac3844b1e060bdb797368d213
GuilhermeDanzer <guidanzer@hotmail.com>
2023-07-06 16:39:40 -0300

    Refactor and optimize RecommendationEngine class

--- app/services/recommendation_engine.rb
+++ app/services/recommendation_engine.rb
@@ -4,20 +4,30 @@ class RecommendationEngine
   end
 
   def recommendations
-    movie_titles = get_movie_names(@favorite_movies)
-    genres = Movie.where(title: movie_titles).pluck(:genre)
-    common_genres = genres.group_by{ |e| e }.sort_by{ |k, v| -v.length }.map(&:first).take(3)
-    Movie.where(genre: common_genres).order(rating: :desc).limit(10)
+    movie_titles = extract_movie_titles(@favorite_movies)
+    genres = fetch_movie_genres(movie_titles)
+    common_genres = determine_common_genres(genres)
+    retrieve_top_rated_movies(common_genres)
   end
 
   private
 
-  def get_movie_names(movies)
-    names = []
-    @favorite_movies.each do |movie|
-      names << movie.title
-    end
+  def extract_movie_titles(movies)
+    movies.map(&:title)
+  end
 
-    return names
+  def fetch_movie_genres(titles)
+    Movie.where(title: titles).pluck(:genre)
+  end
+
+  def determine_common_genres(genres)
+    genre_counts = genres.tally
+    sorted_genres = genre_counts.sort_by { |_genre, count| -count }
+    top_genres = sorted_genres.map(&:first).take(3)
+    top_genres
+  end
+
+  def retrieve_top_rated_movies(genres)
+    Movie.where(genre: genres).order(rating: :desc).limit(10)
   end
 end
